e4c16fde68ea62ec65358f71f104306b0344a508
Ubuntu <issei0813star@gmail.com>
2021-07-25 14:39:33 +0000

    登録用フォーム完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 登録成功時
+      redirect_to @user
+    else
+      # 登録失敗時
+      render "new"
+    end
+      　  
+  end
+  
+  def show
+  end
+  
+  private
+    
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
 end
