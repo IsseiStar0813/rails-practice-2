fc87d1644e2c104f9e19388203c384a22e12cf0a
Ubuntu <issei0813star@gmail.com>
2021-07-26 15:18:46 +0000

    ログイン失敗時の処理

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,15 @@ class SessionsController < ApplicationController
   def new
   end
   
-  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン処理
+      redirect_to user
+    else
+      # ログイン失敗時処理
+      flash.now[:danger] = "メールアドレスまたはパスワードが違います。"
+      render "new"
+    end
+  end
 end
