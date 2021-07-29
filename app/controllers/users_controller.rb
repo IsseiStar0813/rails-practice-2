db0a66573efac6f0370bb6229a0dc271c6294b24
Ubuntu <issei0813star@gmail.com>
2021-07-29 17:51:59 +0000

    ユーザー削除機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to root_url
+  end
+  
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -38,6 +43,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  
   
   private
     
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "/about", to: "static_pages#about"
   get "/signup", to: "users#new"
-  resources:users
+  resources:users, except: [:index]
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
