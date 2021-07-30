4fb3b17229c22573fa1fce6a490171397d2d09ca
Ubuntu <issei0813star@gmail.com>
2021-07-30 09:10:33 +0000

    yu-za-sakujokinou

---
+++ test/integration/users_delete_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class UsersDeleteTest < ActionDispatch::IntegrationTest
+  
+  def setup
+   @user = users(:issei)
+  end
+
+ test "delete user" do
+   log_in_as(@user)
+   get edit_user_path(@user)
+   assert_template 'users/edit'
+   assert_select "a[href=?]",user_path, method: :delete
+   assert_difference 'User.count', -1 do
+      delete user_path
+    end
+   assert_redirected_to root_url
+ end
+
+
+end
