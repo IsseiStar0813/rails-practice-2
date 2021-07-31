093de06770830207ea18458830cf7e213f652e25
Ubuntu <issei0813star@gmail.com>
2021-07-31 17:02:14 +0000

    投稿の順序付け

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -24,6 +24,10 @@ class MicropostTest < ActiveSupport::TestCase
   test "content should be at most 140 characters" do
     @micropost.content = "a" * 141
     assert_not @micropost.valid?
-  end  
-
+  end
+  
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
+  end
+  
 end
