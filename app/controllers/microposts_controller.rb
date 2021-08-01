2ce6789e6182a89010cc9b0dde5d2c663a2e1a2a
Ubuntu <issei0813star@gmail.com>
2021-08-01 15:27:50 +0000

    マイクロポフト投稿用のフォ０無

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,7 +2,6 @@ class MicropostsController < ApplicationController
   
   before_action :logged_in_user, only:[:create, :destroy]
   
-  
   def create
   end
   
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @micropost = current_user.microposts.build if session[:user_id]
   end
 
   def about
