566c6ef81723e6f39f228186a387cb50f3bae7ca
Ubuntu <issei0813star@gmail.com>
2021-08-01 15:16:32 +0000

    beforeフィルターでmicrpostsのcreateとdestroyをアクセス制御

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,19 +61,5 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password)
     end
     
-    # ログイン済みのユーザーか確認
-    def logged_in_user
-      unless session[:user_id]
-        flash[:warning] = "ログインしてください"
-        redirect_to login_url
-      end
-    end
     
-    # 正しいユーザーか確認
-    def correct_user
-      @user = User.find(params[:id])
-      unless @user == current_user
-        redirect_to login_url
-      end
-    end
 end
